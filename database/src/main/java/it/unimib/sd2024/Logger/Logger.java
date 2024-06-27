5cf11d735f7ad1c554fc213dd259427d8e9ae25b
vano2903 <davidevanoncini2003@gmail.com>
2024-06-27 20:30:29 +0200

    feat: add timestamp to logs

--- database/src/main/java/it/unimib/sd2024/Logger/Logger.java
+++ database/src/main/java/it/unimib/sd2024/Logger/Logger.java
@@ -1,15 +1,33 @@
 package it.unimib.sd2024.Logger;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class Logger {
     private final LogLevelType level;
+    private final boolean withTimestamp;
+    private final TimeZone tz;
+    private final DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
 
     public Logger(LogLevelType level) {
+        this(level, true, TimeZone.getDefault());
+    }
+
+    public Logger(LogLevelType level, boolean withTimestamp, TimeZone tz) {
         this.level = level;
+        this.withTimestamp = withTimestamp;
+        this.tz = tz;
+        df.setTimeZone(this.tz);
     }
 
     public void log(LogLevelType level, String message) {
         if (level.getLevel() >= this.level.getLevel()) {
-            System.out.println("[" + LogLevelType.fromLevelToString(level.getLevel()) + "] " + message);
+            if (withTimestamp)
+                System.out.println(LogLevelType.fromLevelToString(level.getLevel()) + "[" + df.format(new Date()) + "] " + message);
+            else
+                System.out.println(LogLevelType.fromLevelToString(level.getLevel()) + "[0000]" + message);
         }
     }
 
